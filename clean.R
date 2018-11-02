dbc891510b777e297fa2629abaa3d6e9dab94975
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:43:07 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,3 +2,53 @@
 clean_data <- raw_data
 clean_data
 data0<- clean_data
+data0<- data123[,-4]
+
+data0<-data0[,-13]
+
+
+
+data0<-data0[,-26]
+
+
+
+data0<-data0[,-25]
+
+
+data0<- data0[,-17]
+
+
+data0<- data0[,-18]
+
+
+data1<-data0[,10]
+
+
+data0<-data0[,-10]
+
+
+
+
+
+v<-data0[data0$Eating.and.Drinking.at.Airport]
+createFuncition<- function(v)
+{
+  low<- quantile(v,0.25)
+  average<- quantile(v,0.50)
+  high<- quantile(v,1)
+  vcat<-v
+  vcat[1:length(v)]<-"average"
+  vcat[v<=low]<-"low"
+  vcat[v>low & v<=average]<-"average"
+  vcat[v>average & v<=high]<- "high"
+  return(vcat)
+  }
+
+ quality<-createFuncition(v)
+   
+
+   data0<- data.frame(data0, quality)
+
+
+data0<- data0[-c(21:22)]
+
