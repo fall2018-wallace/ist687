df4685365e7e0ea61e092f95d1c7d551b6a25eba
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:54:07 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -30,7 +30,7 @@ data0<-data0[,-10]
 data0
 
 
-v<-data0[data0$Eating.and.Drinking.at.Airport]
+v<-data0[,data0$Eating.and.Drinking.at.Airport]
 createFuncition<- function(v)
 {
   low<- quantile(v,0.25)
