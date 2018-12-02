8ecd2ceda68bd778d1cbcf1ef0083b026395d3cd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 21:50:30 -0500

    Saving data_on_flights.R

--- data_on_flights.R
+++ data_on_flights.R
@@ -2,5 +2,6 @@
 data_on_flights<-data01
 data_on_flights
 
+data_on_flights$Flight.time.in.minutes <- as.character(data_on_flights$Flight.time.in.minutes)
 data_on_flights <- data_on_flights[!is.na(data_on_flights$Flight.time.in.minutes),]
 
