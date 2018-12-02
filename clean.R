fd325f8df813bfa9cdfa66ccbda4cebc77511e0f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 23:54:54 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -34,27 +34,9 @@ data0
 v<-data0[,21]
 v
 
-data0$Eating
-createFuncition<- function(v)
-{
-  low<- quantile(v,0.25)
-  average<- quantile(v,0.50)
-  high<- quantile(v,1)
-  vcat<-v
-  vcat[1:length(v)]<-"average"
-  vcat[v<=low]<-"low"
-  vcat[v>low & v<=average]<-"average"
-  vcat[v>average & v<=high]<- "high"
-  return(vcat)
-  }
-
- quality<-createFuncition(v)
-   
-
-   data0<- data.frame(data0, quality)
 
 
-data0<- data0[-c(21:22)]
+data0<- data0[-c(21:22)] #extra arrival delay greater than 5 mins
 
 
 
