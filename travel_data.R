8f8e4d796a395208201f12e4753a6830d2b3717e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 20:32:22 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -2,4 +2,4 @@
 #data00
 traveldata<-data00
 traveldata
-lm1<-scatter.smooth(x=traveldata$Satisfaction, y=traveldata$Flight.time.in.minutes, main="Custsat ~ Flighttimeinminutes") 
+hist(traveldata$Satisfaction)
