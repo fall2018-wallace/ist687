f34c13614b46c37971e9733c5d6b14cec9d5cfd6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 18:38:28 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -179,9 +179,9 @@ a30<-a30+theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle("Satis
 a30
 
 #Flight.cancelled vs Satisfaction
-a31<-ggplot(data=datasatbyname, aes(Departure.Delay.in.Minutes,Satisfaction)) + geom_col()
+a31<-ggplot(data=datasatbyname, aes(Flight.cancelled,Satisfaction)) + geom_col()
 a32<-a31+geom_line()
-a32<-a32+theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle("Satisfaction per Departure.Delay.in.Minutes")
+a32<-a32+theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle("Satisfaction per Flight.cancelled")
 a32
 
 #Flight.time.in.minutes vs Satisfaction
