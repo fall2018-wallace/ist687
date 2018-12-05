77b00b425014e81bb8cfacf9bf7033646de82bd4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 15:27:34 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -1,8 +1,8 @@
 
 #data00
 traveldata<-data00
-traveldata$Departure.Delay.in.Minutes <- ifelse(is.na(sw$Departure.Delay.in.Minutes) & sw$Flight.cancelled == "Yes", 9999, sw$Departure.Delay.in.Minutes)
-traveldata$Arrival.Delay.in.Minutes <- ifelse(is.na(sw$Arrival.Delay.in.Minutes) & sw$Flight.cancelled == "Yes", 9999, sw$Arrival.Delay.in.Minutes)
-traveldataFlight.time.in.minutes <- ifelse(is.na(sw$Flight.time.in.minutes) & sw$Flight.cancelled == "Yes", 9999, sw$Flight.time.in.minutes)
+traveldata$Departure.Delay.in.Minutes <- ifelse(is.na(traveldata$Departure.Delay.in.Minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Departure.Delay.in.Minutes)
+traveldata$Arrival.Delay.in.Minutes <- ifelse(is.na(traveldata$Arrival.Delay.in.Minutes) & straveldataFlight.cancelled == "Yes", 9999, traveldata$Arrival.Delay.in.Minutes)
+traveldata$Flight.time.in.minutes <- ifelse(is.na(traveldata$Flight.time.in.minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Flight.time.in.minutes)
 # df<- sw[-(is.na(sw$Arrival.Delay.in.Minutes)),] df<- na.omit(sw) # (is.na(sw$Arrival.Delay.in.Minutes)) summary(df) str(df) View(df) head(order(-df$X..of.Flight.with.other.Airlines)) write.csv(df, file= 'cleanSatisfactionData.csv') if(sw$Flight.cancelled = 'Yes') { is.na() } 
 summary(data00)
