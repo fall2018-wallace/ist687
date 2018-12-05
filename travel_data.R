ae4204980bd4fa92aa1daeece050c29b6c9e7323
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 15:35:25 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -8,3 +8,15 @@ traveldatacleaned<- traveldata[-(is.na(traveldata$Arrival.Delay.in.Minutes)),]
 traveldatacleaned<- na.omit(traveldata) 
 traveldatacleaned$Satisfaction<-as.numeric(traveldatacleaned$Satisfaction)
  summary(traveldatacleaned) 
+ 
+modelOfAirlineStatus<-lm(formula = Satisfaction~Airline.Status, data = traveldatacleaneddataCleaned)
+# summary(modelOfAirlineStatus)
+# 
+# modelOfAge<-lm(formula = Satisfaction~Age, data = dataCleaned)
+# summary(modelOfAge)
+# 
+# modelOfAge<-lm(formula = Satisfaction~Age, data = dataCleaned)
+# summary(modelOfAge)
+
+# modelOfInfluencingFactors<-lm(formula = Satisfaction ~ Arrival_Delay_greater_5_Mins+Flight_Distance+Flight_time_in_minutes+Flight_cancelled+Arrival_Delay_in_Minutes+Departure_Delay_in_Minutes+Scheduled_Departure_Hour+Destination_State+Destination_City+Origin_State+Orgin_City+Flight_date+Day_of_Month+Class+Eating_and_Drinking_at_Airport+Shopping_Amount_at_Airport+No__of_other_Loyalty_Cards+Type_of_Travel+X__of_Flight_with_other_Airlines+No_of_Flights_p_a_+Year_of_First_Flight+Price_Sensitivity+Gender+Age+Airline_Status, data = dataCleaned)
+# summary(modelOfInfluencingFactors)
