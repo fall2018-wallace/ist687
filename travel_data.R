33a78c1eceb67750fd2d370f8259bc9973ca3ce4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 15:33:38 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -4,8 +4,8 @@ traveldata<-data00
 traveldata$Departure.Delay.in.Minutes <- ifelse(is.na(traveldata$Departure.Delay.in.Minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Departure.Delay.in.Minutes)
 traveldata$Arrival.Delay.in.Minutes <- ifelse(is.na(traveldata$Arrival.Delay.in.Minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Arrival.Delay.in.Minutes)
 traveldata$Flight.time.in.minutes <- ifelse(is.na(traveldata$Flight.time.in.minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Flight.time.in.minutes)
-df<- traveldata[-(is.na(traveldata$Arrival.Delay.in.Minutes)),] 
-df<- na.omit(traveldata) 
-  
-traveldata$Satisfaction<-as.numeric(traveldata$Satisfaction)
- summary(df) #$str(df) View(df)
+traveldatacleaned<- traveldata[-(is.na(traveldata$Arrival.Delay.in.Minutes)),] 
+traveldatacleaned<- na.omit(traveldata) 
+ 
+traveldatacleaned$Satisfaction<-as.numeric(traveldatacleaned$Satisfaction)
+ summary(traveldatacleaned) 
