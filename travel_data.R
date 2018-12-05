ddb08a237c42084b76f59bd7d0ab3b4bee934f72
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 15:27:45 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -5,4 +5,4 @@ traveldata$Departure.Delay.in.Minutes <- ifelse(is.na(traveldata$Departure.Delay
 traveldata$Arrival.Delay.in.Minutes <- ifelse(is.na(traveldata$Arrival.Delay.in.Minutes) & straveldataFlight.cancelled == "Yes", 9999, traveldata$Arrival.Delay.in.Minutes)
 traveldata$Flight.time.in.minutes <- ifelse(is.na(traveldata$Flight.time.in.minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Flight.time.in.minutes)
 # df<- sw[-(is.na(sw$Arrival.Delay.in.Minutes)),] df<- na.omit(sw) # (is.na(sw$Arrival.Delay.in.Minutes)) summary(df) str(df) View(df) head(order(-df$X..of.Flight.with.other.Airlines)) write.csv(df, file= 'cleanSatisfactionData.csv') if(sw$Flight.cancelled = 'Yes') { is.na() } 
-summary(data00)
+summary(traveldata)
