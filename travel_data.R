fca5676caa1b28576a63cec3ab828d788ecae11b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 15:16:58 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -2,8 +2,8 @@
 #data00
 traveldata<-data00
 #traveldata
-traveldata$Satisfaction <- as.numeric(as.character(traveldata$Satisfaction))
-traveldata$Satisfaction
+#traveldata$Satisfaction <- as.numeric(as.character(traveldata$Satisfaction))
+#traveldata$Satisfaction
 #lmFlight <- lm(formula=Satisfaction~Scheduled.Departure.Hour+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Flight.time.in.minutes, data = data00
 #summary(lmFlight)
 
