314817ab0f447d5a97f76bc36480ece34c568492
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 19:55:30 -0500

    Saving travel_data.R

--- travel_data.R
+++ travel_data.R
@@ -1,6 +1,6 @@
 
 #data00
-traveldata<-data00
+traveldata<-raw_data
 traveldata$Departure.Delay.in.Minutes <- ifelse(is.na(traveldata$Departure.Delay.in.Minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Departure.Delay.in.Minutes)
 traveldata$Arrival.Delay.in.Minutes <- ifelse(is.na(traveldata$Arrival.Delay.in.Minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Arrival.Delay.in.Minutes)
 traveldata$Flight.time.in.minutes <- ifelse(is.na(traveldata$Flight.time.in.minutes) & traveldata$Flight.cancelled == "Yes", 9999, traveldata$Flight.time.in.minutes)
